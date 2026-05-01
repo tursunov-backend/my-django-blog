f67fcd38d738552bec82aeaeafb7ebd6a48b7dba
tursunov-backent <donitursunov98@gmail.com>
2026-05-01 21:51:14 +0500

    template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import  HttpRequest, HttpResponse
 
-# Create your views here.
+def home_page(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name='home.html')
+
+def contact_page(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name='contact.html')
