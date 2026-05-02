c47e438be3f6013d6af8e491868174c5b1df06fa
tursunov-backent <donitursunov98@gmail.com>
2026-05-02 12:04:26 +0500

    html

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('contact/', views.contact_page, name='contact'),
-
+    path('blog_list/', views.blog_list, name='blog-list'),
+    path('boglanish/', views.contact_page, name='contact'),
+    path('haqimda/', views.about_page, name='about'),
+    path('loyihalar/', views.projects_page, name='projects'),
+    path('maqola/<int:pk>/', views.blog_detail, name='blog-detail'),
+    path('maqola/yarat/', views.blog_create, name='blog-create'),
+    path('maqola/<int:pk>/tahrir/', views.blog_edit, name='blog-edit'),
+    path('maqola/<int:pk>/ochir/', views.blog_delete, name='blog-delete'),  
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
-from django.http import  HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpRequest, HttpResponse
+
 
 def home_page(request: HttpRequest) -> HttpResponse:
     return render(request=request, template_name='home.html')
 
+def blog_list(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name='blog_list.html')
+
 def contact_page(request: HttpRequest) -> HttpResponse:
     return render(request=request, template_name='contact.html')
+
+def about_page(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name='about.html')
+
+def projects_page(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name='projects.html')
+
+def blog_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    return render(request=request, template_name='blog_detail.html')
+
+def blog_create(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name='blog_create.html')
+
+def blog_edit(request: HttpRequest, pk: int) -> HttpResponse:
+    return render(request=request, template_name='blog_edit.html')
+
+def blog_delete(request: HttpRequest, pk: int) -> HttpResponse:
+    return render(request=request, template_name='blog_delete.html')
